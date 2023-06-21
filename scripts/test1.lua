54b9e2681e0e6de4081ede6602135d794b75f99c
Rexi <68111233+RexiRexii@users.noreply.github.com>
2023-06-21 17:11:01 +0300

    Update test1.lua

--- scripts/test1.lua
+++ scripts/test1.lua
@@ -67,7 +67,7 @@ getgenv().bit = bit
 getgenv().bit32 = bit
 
 setreadonly(bit, true)
-
+                                             
 local VirtualInputManager = Instance.new("VirtualInputManager")
 local GuiService = cloneref(game:GetService("GuiService"))
 
