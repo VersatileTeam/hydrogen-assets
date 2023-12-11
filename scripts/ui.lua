63bc088125475fd2a43b1e5cf78b39d286671142
Rexi <68111233+RexiRexii@users.noreply.github.com>
2023-12-12 01:50:40 +0300

    Update ui.lua

--- scripts/ui.lua
+++ scripts/ui.lua
@@ -4,11 +4,12 @@ local renv = clonefunction(getrenv)()
 protectfunction(renv)
 local _wait = clonefunction(renv.wait)
 protectfunction(_wait)
+local _loadstring = clonefunction(loadstring)
+protectfunction(_loadstring)
 
 repeat _wait() until game:IsLoaded()
-print(game:IsLoaded())
-
-local name = identifyexecutor()
+local name = clonefunction(identifyexecutor)()
+protectfunction(name)
 
 local UI = {
     Hydrogen = "https://projectevo.xyz/ui.lua",
@@ -16,14 +17,16 @@ local UI = {
     Codex = "https://cdn.codex.lol/public/main.txt",
     Evon = "https://gist.githubusercontent.com/RexiRexii/edf28d8d680ee05af7c025d4f6f0f886/raw/2682f6291f2c7a77c9f101fbd7f47ac8eed2c5b4/evon.lua"
 }
+protectfunction(ui)
 
 local selected_ui = UI[name];
+protectfunction(selected_ui)
 
 -- lenny told me wait(1) was a good idea, i highly disagree but ok -rexi
 if selected_ui then
     _wait(1)
-    loadstring(game:HttpGet(selected_ui, true))()
+    _loadstring(game:HttpGet(selected_ui, true))()
 else
     _wait(1)
-    loadstring(game:HttpGet("https://projectevo.xyz/ui.lua", true))()
+    _loadstring(game:HttpGet("https://projectevo.xyz/ui.lua", true))()
 end
