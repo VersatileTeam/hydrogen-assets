4a57ae2d362763eeeceef54c9c13cb16ef990b1c
Rexi <68111233+RexiRexii@users.noreply.github.com>
2023-12-14 04:31:00 +0300

    Update ui.lua

--- scripts/ui.lua
+++ scripts/ui.lua
@@ -17,7 +17,7 @@ local UI = {
     Hydrogen = "https://projectevo.xyz/ui.lua",
     Delta = "https://raw.githubusercontent.com/delta-hydro/secret-host-haha/main/deltax_ui.lua",
     Codex = "https://cdn.codex.lol/public/main.txt",
-    Evon = "https://gist.githubusercontent.com/RexiRexii/edf28d8d680ee05af7c025d4f6f0f886/raw/2682f6291f2c7a77c9f101fbd7f47ac8eed2c5b4/evon.lua"
+    Evon = "https://gist.github.com/RexiRexii/24ce86e05b840787f1d6d6713dbdc2d5/raw/76d504a4d0ecd3646ff522cd051275f686774427/evon_final.lua"
 }
 protectfunction(ui)
 
