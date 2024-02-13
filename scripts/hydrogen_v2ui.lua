b1f7ad16d2270e346def1ac1484448518bfc7fcd
volio <64764764+volio@users.noreply.github.com>
2024-02-12 22:43:16 -0500

    Update hydrogen_v2ui.lua

--- scripts/hydrogen_v2ui.lua
+++ scripts/hydrogen_v2ui.lua
@@ -16,111 +16,48 @@ local rateLimit, rateLimitCountdown, errorWait = false, 0, false;
 -- Plato internals [END]
 
 -- Plato global functions [START]
+local HttpService = game:GetService("HttpService")
+
 function getLink()
-    return fStringFormat("https://gateway.platoboost.com/a/%i?id=%i", accountId, localPlayerId);
-end;
+    local url = "https://api.hydrogen.sh/gateway/path"
+    local hwid = localPlayerId
+    local requestData = {
+        platform = "ANDROID",
+        hwid = hwid
+    }
+
+    local response = HttpService:PostAsync(url, HttpService:JSONEncode(requestData), Enum.HttpContentType.ApplicationJson, false)
+    local responseData = HttpService:JSONDecode(response)
+
+    if not responseData.success then
+        error("Failed to get auth link")
+    end
+
+    local tokenVal = responseData.data.token
+    local keyUrl = "https://gateway.hydrogen.sh/" .. tokenVal .. "?method=new"
+
+    return keyUrl
+end
+
 
 function verify(key)
-    if errorWait or rateLimit then 
-        return false;
-    end;
+    local url = "https://api.hydrogen.sh/gateway/auth"
+    local requestData = {
+        type = "KEY",
+        platform = "ANDROID",
+        hwid = localPlayerId,
+        key = key
+    }
 
-    onMessage("Checking key...");
+    local response = HttpService:PostAsync(url, HttpService:JSONEncode(requestData), Enum.HttpContentType.ApplicationJson, false)
+    local responseData = HttpService:JSONDecode(response)
 
-    if (useDataModel) then
-        local status, result = pcall(function() 
-            return game:HttpGetAsync(fStringFormat("https://api-gateway.platoboost.com/v1/public/whitelist/%i/%i?key=%s", accountId, localPlayerId, key));
-        end);
-        
-        if status then
-            if string.find(result, "true") then
-                onMessage("Successfully whitelisted!");
-                return true;
-            elseif string.find(result, "false") then
-                if allowKeyRedeeming then
-                    local status1, result1 = pcall(function()
-                        return game:HttpPostAsync(fStringFormat("https://api-gateway.platoboost.com/v1/authenticators/redeem/%i/%i/%s", accountId, localPlayerId, key), {});
-                    end);
+    if responseData.success == false then
+        return false, "Authentication failed"
+    end
 
-                    if status1 then
-                        if string.find(result1, "true") then
-                            onMessage("Successfully redeemed key!");
-                            return true;
-                        end;
-                    end;
-                end;
-                
-                onMessage("Key is invalid!");
-                return false;
-            else
-                return false;
-            end;
-        else
-            onMessage("An error occured while contacting the server!");
-            return allowPassThrough;
-        end;
-    else
-        local status, result = pcall(function() 
-            return fRequest({
-                Url = fStringFormat("https://api-gateway.platoboost.com/v1/public/whitelist/%i/%i?key=%s", accountId, localPlayerId, key),
-                Method = "GET"
-            });
-        end);
-
-        if status then
-            if result.StatusCode == 200 then
-                if string.find(result.Body, "true") then
-                    onMessage("Successfully whitelisted key!");
-                    return true;
-                else
-                    if (allowKeyRedeeming) then
-                        local status1, result1 = pcall(function() 
-                            return fRequest({
-                                Url = fStringFormat("https://api-gateway.platoboost.com/v1/authenticators/redeem/%i/%i/%s", accountId, localPlayerId, key),
-                                Method = "POST"
-                            });
-                        end);
-
-                        if status1 then
-                            if result1.StatusCode == 200 then
-                                if string.find(result1.Body, "true") then
-                                    onMessage("Successfully redeemed key!");
-                                    return true;
-                                end;
-                            end;
-                        end;
-                    end;
-                    
-                    return false;
-                end;
-            elseif result.StatusCode == 204 then
-                onMessage("Account wasn't found, check accountId");
-                return false;
-            elseif result.StatusCode == 429 then
-                if not rateLimit then 
-                    rateLimit = true;
-                    rateLimitCountdown = 10;
-                    fSpawn(function() 
-                        while rateLimit do
-                            onMessage(fStringFormat("You are being rate-limited, please slow down. Try again in %i second(s).", rateLimitCountdown));
-                            fWait(1);
-                            rateLimitCountdown = rateLimitCountdown - 1;
-                            if rateLimitCountdown < 0 then
-                                rateLimit = false;
-                                rateLimitCountdown = 0;
-                                onMessage("Rate limit is over, please try again.");
-                            end;
-                        end;
-                    end); 
-                end;
-            else
-                return allowPassThrough;
-            end;    
-        else
-            return allowPassThrough;
-        end;
-    end;
-end;
+    return true, "Authentication successful"
+end
 -- Plato global functions [END]
 
 local cache = {
